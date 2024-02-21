b0dfd4b98404970aa74db4f40f003747555938e6
ptdrdheeraj <ptdrdheeraj22@gmail.com>
2024-02-21 18:44:45 +0530

    done

---
+++ PROJECTORS/projectORS/src/main/java/com/rays/dao/SubjectDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.SubjectDTO;
+/**
+ * @author Dheeraj Patidar
+ *
+ */
+public interface SubjectDAOInt extends BaseDAOInt<SubjectDTO>{
+
+}
---
+++ PROJECTORS/projectORS/src/main/java/com/rays/dao/TimeTableDAOInt.java
@@ -0,0 +1,12 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.TimeTableDTO;
+/**
+ * @author Dheeraj Patidar
+ *
+ */
+public interface TimeTableDAOInt extends BaseDAOInt<TimeTableDTO> {
+
+	
+}
